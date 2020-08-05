9a12c36204bfb830c66849b79687adb81483b858
Justin Lohner <lohner4@gmail.com>
2020-08-05 13:23:36 -0600

    wrote expensive_seq function and got it passing tests

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,14 +1,15 @@
-# Your code here
-
+mem = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
-
-
+	if x <= 0:
+		mem[(x, y, z)] = (y + z)
+	if (x, y, z) not in mem:
+		mem[(x, y, z)] = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+	return mem[(x, y, z)]
 
 if __name__ == "__main__":
-    for i in range(10):
-        x = expensive_seq(i*2, i*3, i*4)
-        print(f"{i*2} {i*3} {i*4} = {x}")
+	for i in range(10):
+		x = expensive_seq(i*2, i*3, i*4)
+		print(f"{i*2} {i*3} {i*4} = {x}")
 
-    print(expensive_seq(150, 400, 800))
+	print(expensive_seq(150, 400, 800))
