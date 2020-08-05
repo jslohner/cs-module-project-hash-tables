43e81432d45f289d4027dc2f2ae8164a947ad3e3
Justin Lohner <lohner4@gmail.com>
2020-08-05 15:22:46 -0600

    wrote no_dups function, no_dups tests now passing

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,11 +1,16 @@
 def no_dups(s):
-    # Your code here
-
-
+	mem = {}
+	word_list = s.strip().split(' ')
+	rtn_str = ''
+	for i in range(len(word_list)):
+		if word_list[i] not in mem:
+			mem[word_list[i]] = word_list[i]
+			rtn_str += f' {word_list[i]}' if i != 0 else f'{word_list[i]}'
+	return rtn_str
 
 if __name__ == "__main__":
-    print(no_dups(""))
-    print(no_dups("hello"))
-    print(no_dups("hello hello"))
-    print(no_dups("cats dogs fish cats dogs"))
-    print(no_dups("spam spam spam eggs spam sausage spam spam and spam"))
+	print(no_dups(""))
+	print(no_dups("hello"))
+	print(no_dups("hello hello"))
+	print(no_dups("cats dogs fish cats dogs"))
+	print(no_dups("spam spam spam eggs spam sausage spam spam and spam"))
