7ff36efab7129482ed1a760f046ff06d7b06d7a5
Justin Lohner <lohner4@gmail.com>
2020-08-05 14:29:45 -0600

    wrote word_count function, word_count tests now passing

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,16 @@
+import re
+
 def word_count(s):
-    # Your code here
-
-
+	mem = {}
+	word_list = list(filter(None, (re.split('[\\\\\-\[\]\":;,.+=/|{}()*^&\s]', s.lower()))))
+	for word in word_list:
+		if word not in mem:
+			mem[word] = 0
+		mem[word] += 1
+	return mem
 
 if __name__ == "__main__":
-    print(word_count(""))
-    print(word_count("Hello"))
-    print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+	print(word_count(""))
+	print(word_count("Hello"))
+	print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
+	print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
