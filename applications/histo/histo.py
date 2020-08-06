a658a8edf3f07e59b5d14a4ca3652c6b4b7a5f66
Justin Lohner <lohner4@gmail.com>
2020-08-06 13:21:44 -0600

    finished the histogram word count file

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,21 @@
-# Your code here
+import re
 
+def printWordHistogram(filename):
+	mem = {}
+	longestStr = 0
+	with open(filename, 'r') as f:
+		# print(f.readlines())
+		for line in f:
+			wordList = list(filter(None, (re.split('[\\\\\-\[\]\":;,.+=/|{}()*^&\s]', line.lower()))))
+			for word in wordList:
+				word = re.sub(r'[^\w\s]', '', word)
+				if len(word) > longestStr:
+					longestStr = len(word)
+				if word not in mem:
+					mem[word] = ''
+				mem[word] += '#'
+	mem = {k:v for k,v in sorted(mem.items(), key=lambda item: (-len(item[1]), item[0]))}
+	for key in mem:
+		print(f'{key:<{longestStr + 2}}{mem[key]}')
+
+printWordHistogram('robin.txt')
